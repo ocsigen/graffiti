ae5e67aa1ac632500e91da35d4973d86b72c749e
Enguerrand Decorne <decorne.en@gmail.com>
2014-05-21 14:08:50 +0200

    Remove now useless function in graffiti-extended

--- extended/server.ml
+++ extended/server.ml
@@ -27,12 +27,6 @@ module My_app =
     let application_name = "graffiti"
   end)
 
-let rgb_from_string color = (* color is in format "#rrggbb" *)
-  let get_color i =
-    (float_of_string ("0x"^(String.sub color (1+2*i) 2))) /. 255.
-  in
-  try get_color 0, get_color 1, get_color 2 with | _ -> 0., 0., 0.
-
 let launch_server_canvas () =
   let bus = Eliom_bus.create Json.t<messages> in
 
